34382bb788f37e213c6e6ca8de2ae50254aca7f6
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2025-01-18 11:37:56 +0530

    ..

--- client/config/site.ts
+++ client/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Karbon Store",
+  description: "Marketplace for karbon credit tokens.",
   navItems: [
     {
       label: "Home",
@@ -26,6 +26,10 @@ export const siteConfig = {
     },
   ],
   navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
     {
       label: "Profile",
       href: "/profile",
@@ -60,10 +64,6 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/frontio-ai/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github: "https://github.com/KonmaORG/karbon-marketplace",
   },
 };
