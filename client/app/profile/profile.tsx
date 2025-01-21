1cbd24772a8b26cea40e4555283f363518fc8bf1
Cryptonean <faizanvit8@gmail.com>
2025-01-21 12:15:05 +0530

    profile refactor

--- client/app/profile/profile.tsx
+++ client/app/profile/profile.tsx
@@ -1,61 +1,60 @@
 "use client";
-import TokenCard from "@/components/tokenCard";
-import { POLICYID } from "@/config";
-import { emulator } from "@/config/emulator";
-import { useWallet } from "@/context/walletContext";
 import React, { useEffect, useState } from "react";
 
+import TokenCard from "@/components/tokenCard";
+import { POLICYID } from "@/config";
+import { useWallet } from "@/context/walletContext";
+
 export default function Profile() {
-    const [walletConnection, setWalletConnection] = useWallet();
-    const { wallet, lucid, address } = walletConnection;
-    const [karbonTokens, setKarbonTokens] = useState<Record<string, number>>({});
-    useEffect(() => {
-        async function tokens() {
-            if (!lucid || !address) return;
-            const assetSet = new Set();
-            const utxos = await lucid.wallet().getUtxos();
-            // const utxos = await emulator.getUtxos(address);
-            const aggregateTokenQuantities = () => {
-                const quantities: Record<string, number> = {};
+  const [walletConnection] = useWallet();
+  const { wallet, lucid, address } = walletConnection;
+  const [karbonTokens, setKarbonTokens] = useState<Record<string, number>>({});
 
-                utxos.forEach((utxo) => {
-                    if (utxo.assets) {
-                        Object.entries(utxo.assets).forEach(([assetKey, quantity]) => {
-                            if (assetKey.startsWith(POLICYID)) {
-                                if (quantities[assetKey as keyof typeof quantities]) {
-                                    quantities[assetKey as keyof typeof quantities] +=
-                                        Number(quantity); // Add quantity if the token exists
-                                } else {
-                                    quantities[assetKey as keyof typeof quantities] =
-                                        Number(quantity); // Initialize the token with its quantity
-                                }
-                            }
-                        });
-                    }
-                });
-                console.log(quantities)
-                setKarbonTokens(quantities);
-            };
+  useEffect(() => {
+    async function tokens() {
+      if (!lucid || !address) return;
+      const utxos = await lucid.wallet().getUtxos();
+      // const utxos = await emulator.getUtxos(address);
+      const aggregateTokenQuantities = () => {
+        const quantities: Record<string, number> = {};
 
-            aggregateTokenQuantities();
-        }
-        tokens();
-    }, [address]);
+        utxos.forEach((utxo) => {
+          if (utxo.assets) {
+            Object.entries(utxo.assets).forEach(([assetKey, quantity]) => {
+              if (assetKey.startsWith(POLICYID)) {
+                if (quantities[assetKey as keyof typeof quantities]) {
+                  quantities[assetKey as keyof typeof quantities] +=
+                    Number(quantity); // Add quantity if the token exists
+                } else {
+                  quantities[assetKey as keyof typeof quantities] =
+                    Number(quantity); // Initialize the token with its quantity
+                }
+              }
+            });
+          }
+        });
+        setKarbonTokens(quantities);
+      };
 
-    useEffect(() => {
-        setKarbonTokens({});
-    }, [address, wallet]);
+      aggregateTokenQuantities();
+    }
+    tokens();
+  }, [address]);
 
-    if (!address)
-        return <div className="mx-auto w-full">Connect Your Wallet First</div>;
-    return (
-        <div className="flex gap-4 flex-wrap">
-            {Object.entries(karbonTokens).map(([token, qty], index) => (
-                <>
-                    <TokenCard key={index} token={token} qty={qty} type="Sell" />
-                </>
-            ))
-            }
-        </div >
-    );
+  useEffect(() => {
+    setKarbonTokens({});
+  }, [address, wallet]);
+
+  if (!address)
+    return <div className="mx-auto w-full">Connect Your Wallet First</div>;
+
+  return (
+    <div className="flex gap-4 flex-wrap">
+      {Object.entries(karbonTokens).map(([token, qty], index) => (
+        <>
+          <TokenCard key={index} qty={qty} token={token} type="Sell" />
+        </>
+      ))}
+    </div>
+  );
 }
